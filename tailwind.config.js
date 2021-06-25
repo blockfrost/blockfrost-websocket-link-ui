7740394723cf12421f32dfd9d136107028311d85
Vladimir Volek <volek.vladimir@gmail.com>
2021-06-25 16:23:03 +0200

    feat: estimate fee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      fontFamily: 'Rubik',
+      fontFamily: "Rubik",
     },
     fontFamily: {
-      sans: ['Rubik'],
+      sans: ["Rubik"],
     },
   },
   variants: {
