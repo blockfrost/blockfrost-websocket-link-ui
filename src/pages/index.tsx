00c04a3489192e574e1d4745e885b0ff942a7cb8
Vladimir Volek <volek.vladimir@gmail.com>
2021-10-06 13:10:40 +0200

    fix: page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Index = (): ReactElement => {
         "6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2",
       balanceHistoryKey:
         "6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2",
-      accountInfoPage: 0,
+      accountInfoPage: 1,
       accountInfoPageSize: 25,
       accountInfoDetails: {
         value: "basic",
